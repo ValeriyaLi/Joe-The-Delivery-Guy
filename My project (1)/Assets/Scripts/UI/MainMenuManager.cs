e7c9f440af6e8d00e5ae67b03770ef22b4f50f22
NurmagambetovBakytzhan <nurmagambetovbakytzan@gmail.com>
2024-12-11 22:09:30 +0500

    added main menu and pause with settings

---
+++ My project (1)/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+public class MainMenuManager : MonoBehaviour
+{
+	
+	[Header("Main Menu")]
+	[SerializeField] private GameObject mainMenuScreen;
+    private void Awake()
+    {
+		mainMenuScreen.SetActive(false);
+    }
+    private void Update()
+    {
+
+    }
+
+    #region Game Over
+    
+	public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    //Quit game/exit play mode if in Editor
+    public void Quit()
+    {
+        Application.Quit(); //Quits the game (only works in build)
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; //Exits play mode (will only be executed in the editor)
+#endif
+    }
+    #endregion
+}
