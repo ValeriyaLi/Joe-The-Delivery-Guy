e5acc814721d9ad26f655bac5be77ec0148c794d
ValeriyaLi <livaleria25@gmail.com>
2024-12-11 17:05:13 +0500

    Melee enemy

--- My project (1)/Assets/Scripts/Health/Health.cs
+++ My project (1)/Assets/Scripts/Health/Health.cs
@@ -27,7 +27,21 @@ public class Health : MonoBehaviour
             if (!dead)
             {
                 anim.SetTrigger("die");
-                GetComponent<PlayerMovement>().enabled = false;
+                if (GetComponent<PlayerMovement>() != null)
+                {
+                    GetComponent<PlayerMovement>().enabled = false;
+                }
+
+                if (GetComponentInParent<EnemyPatrol>() != null)
+                {
+                    GetComponentInParent<EnemyPatrol>().enabled = false;
+                }
+
+                if (GetComponent<MeleeEnemy>() != null)
+                {
+                    GetComponent<MeleeEnemy>().enabled = false;
+                }
+                
                 dead = true;
             }
             else 
--- My project (1)/Assets/Scripts/Player/Projectile.cs
+++ My project (1)/Assets/Scripts/Player/Projectile.cs
@@ -30,6 +30,9 @@ public class Projectile : MonoBehaviour
         boxCollider.enabled = false;
 		gameObject.SetActive(false);
         anim.SetTrigger("explode");
+        
+        if (collision.tag == "Enemy")
+            collision.GetComponent<Health>().TakeDamage(1);
     }
     public void SetDirection(float _direction)
     {
